14988c23a815bf13f061a0b112786e67fd43828e
eranelbaz <eranelbaz97@gmail.com>
2021-11-22 12:29:23 +0200

    Chore fix partial (#19)

--- src/transform.ts
+++ src/transform.ts
@@ -5,119 +5,131 @@ import * as JSON5 from "json5";
 const packageName = "env0-ts-transform-json-schema";
 
 export interface TransformerOptions {
-  env: { [key: string]: string };
+    env: { [key: string]: string };
+}
+
+const getFullyQualifiedTypeName = (node) => {
+    const qualifiers = []
+
+    while(node) {
+        // If we don't have right it means we are in the top hierarchy
+        qualifiers.push((node.right ?? node).escapedText);
+        node = node.left
+    }
+    return qualifiers.reverse().join('.');
 }
 
 export const getTransformer = (program: ts.Program) => {
-  const typeChecker = program.getTypeChecker();
+    const typeChecker = program.getTypeChecker();
 
-  function getVisitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
-    const visitor: ts.Visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
-      if (ts.isCallExpression(node)) {
-        if (
-          typeof node.typeArguments === "undefined" ||
-          node.typeArguments.length === 0
-        ) {
-          return node;
-        }
+    function getVisitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
+        const visitor: ts.Visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+            if (ts.isCallExpression(node)) {
+                if (
+                    typeof node.typeArguments === "undefined" ||
+                    node.typeArguments.length === 0
+                ) {
+                    return node;
+                }
 
-        const signature = typeChecker.getResolvedSignature(node);
+                const signature = typeChecker.getResolvedSignature(node);
 
-        if (signature !== undefined && signature.declaration !== undefined) {
-          const sourceName = signature.declaration.getSourceFile().fileName;
+                if (signature !== undefined && signature.declaration !== undefined) {
+                    const sourceName = signature.declaration.getSourceFile().fileName;
 
-          if (!sourceName.includes(packageName)) {
+                    if (!sourceName.includes(packageName)) {
+                        return ts.visitEachChild(node, visitor, ctx);
+                    }
+
+                    const typeArgument = node.typeArguments[0];
+
+                    const type = typeChecker.getTypeFromTypeNode(typeArgument);
+                    const symbol = type.aliasSymbol || type.symbol;
+
+                    const argNode = node.arguments[0];
+                    const options = argNode ? getOptions(argNode) : {
+                        required: true,
+                        noExtraProps: true,
+                        skipTypeCheck: true
+                    };
+
+                    if (typeof symbol === "undefined" || symbol === null) {
+                        throw new Error(`Could not find symbol for passed type`);
+                    }
+
+                    const tsconfig = ctx.getCompilerOptions().configFilePath as string;
+                    const projectApi = tsconfig.replace('tsconfig.json', 'api.d.ts');
+                    const hasApiFile = program.getSourceFiles().map(f => f.fileName).includes(projectApi);
+
+                    if (!hasApiFile) throw 'Project must have an api.d.ts file that includes the type'
+
+                    // @ts-ignore typeName exists in real but not on the type
+                    const namespacedTypeName = getFullyQualifiedTypeName(typeArgument.typeName);
+
+                    const config = {
+                        path: projectApi,
+                        tsconfig,
+                        type: namespacedTypeName,
+                        ...options
+                    };
+
+                    const generator = tjs.createGenerator(config);
+                    const schema = generator.createSchema(config.type);
+
+                    return toLiteral(schema);
+                }
+            }
+
+            if (ts.isImportDeclaration(node)) {
+                const rawSpec = node.moduleSpecifier.getText();
+                const spec = rawSpec.substring(1, rawSpec.length - 1);
+
+                if (spec === packageName) {
+                    return;
+                }
+            }
+
             return ts.visitEachChild(node, visitor, ctx);
-          }
+        };
 
-          const typeArgument = node.typeArguments[0];
+        return visitor;
+    }
 
-          const type = typeChecker.getTypeFromTypeNode(typeArgument);
-          const symbol = type.aliasSymbol || type.symbol;
-
-          const argNode = node.arguments[0];
-          const options = argNode ? getOptions(argNode) : {
-            required: true,
-            noExtraProps: true,
-            skipTypeCheck: true
-          };
-
-          if (typeof symbol === "undefined" || symbol === null) {
-            throw new Error(`Could not find symbol for passed type`);
-          }
-
-          const tsconfig = ctx.getCompilerOptions().configFilePath as string;
-          const projectApi = tsconfig.replace('tsconfig.json', 'api.d.ts');
-          const hasApiFile = program.getSourceFiles().map(f => f.fileName).includes(projectApi);
-
-          if(!hasApiFile) throw 'Project must have an api.d.ts file that includes the type'
-
-          const namespacedTypeName = typeChecker.getFullyQualifiedName(symbol).replace(/".*"\./, "");
-
-          const config = {
-            path: projectApi,
-            tsconfig,
-            type: namespacedTypeName,
-            ...options
-          };
-
-          const generator = tjs.createGenerator(config);
-          const schema = generator.createSchema(config.type);
-
-          return toLiteral(schema);
-        }
-      }
-
-      if (ts.isImportDeclaration(node)) {
-        const rawSpec = node.moduleSpecifier.getText();
-        const spec = rawSpec.substring(1, rawSpec.length - 1);
-
-        if (spec === packageName) {
-          return;
-        }
-      }
-
-      return ts.visitEachChild(node, visitor, ctx);
-    };
-
-    return visitor;
-  }
-
-  return (ctx: ts.TransformationContext): ts.Transformer<ts.SourceFile> => (
-    sf: ts.SourceFile
-  ) => ts.visitNode(sf, getVisitor(ctx, sf));
+    return (ctx: ts.TransformationContext): ts.Transformer<ts.SourceFile> => (
+        sf: ts.SourceFile
+    ) => ts.visitNode(sf, getVisitor(ctx, sf));
 };
 
 // TODO: Factor out, test
 function toLiteral(input: unknown): ts.PrimaryExpression {
-  if (
-    typeof input === "string" ||
-    typeof input === "boolean" ||
-    typeof input === "number"
-  ) {
-    return ts.createLiteral(input);
-  }
+    if (
+        typeof input === "string" ||
+        typeof input === "boolean" ||
+        typeof input === "number"
+    ) {
+        return ts.createLiteral(input);
+    }
 
-  if (typeof input === "object" && Array.isArray(input)) {
-    return ts.createArrayLiteral(input.map(toLiteral));
-  }
+    if (typeof input === "object" && Array.isArray(input)) {
+        return ts.createArrayLiteral(input.map(toLiteral));
+    }
 
-  if (input !== null && typeof input === "object" && !Array.isArray(input)) {
-    const ob = input as object;
-    return ts.createObjectLiteral(
-      Object.keys(ob).map(key =>
-        ts.createPropertyAssignment(ts.createLiteral(key), toLiteral(ob[key]))
-      )
-    );
-  }
+    if (input !== null && typeof input === "object" && !Array.isArray(input)) {
+        const ob = input as object;
+        return ts.createObjectLiteral(
+            Object.keys(ob).map(key =>
+                ts.createPropertyAssignment(ts.createLiteral(key), toLiteral(ob[key]))
+            )
+        );
+    }
 
-  return ts.createNull();
+    return ts.createNull();
 }
 
 function getOptions(node: ts.Node): object {
-  try {
-    return JSON5.parse(node.getText());
-  } catch (err) {
-    return;
-  }
+    try {
+        return JSON5.parse(node.getText());
+    } catch (err) {
+        return;
+    }
 }
